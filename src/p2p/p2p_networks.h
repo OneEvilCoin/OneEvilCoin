d3560e46a7e0e15dc77f0ae265449f87bf2a8b76
OneEvil <oneevil@bk.ru>
2014-07-09 15:09:15 +0600

    First version OneEvilCoin

---
+++ src/p2p/p2p_networks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2014 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace nodetool
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x66 ,0x60, 0x66, 0x60 , 0x66, 0x60 , 0x66, 0x60, 0x66, 0x60, 0x66, 0x60, 0x66, 0x60, 0x66, 0x60} };
+}
